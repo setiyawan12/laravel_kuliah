3d03fc45b5062240f9782d9b9bbcbe5432564ab5
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-03-17 18:25:03 +0700

    1. laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('assets/css/card.css')}}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="card">
+        <div class="imgBox">
+            <img src="{{asset('assets/img/setiyawan.jpeg')}}">
+            <img src="{{asset('assets/img/setiyawan1.jpeg')}}">
+        </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Yayang Setiyawan<br><span>18090045</span></h2>
+                    <div class="social-icons">
+                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="https://github.com/setiyawan12"><i class="fa fa-github" aria-hidden="true"></i></i></a>
+                        <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></i></a>
+                        <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
